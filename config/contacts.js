6f2afc1fe81af6f2cd076dc24aa669b78673b8ff
Abhishek-Budakoti <abhishek.budakoti.04@gmail.com>
2026-07-30 13:07:17 +0530

    Update contact information and office details across multiple files

--- config/contacts.js
+++ config/contacts.js
@@ -1,18 +1,15 @@
 module.exports = {
-  primaryPhone: '+91 88473 56318',
+  primaryPhone: '+91 98211 15848',
   primaryPhoneRaw: '+918847356318',
   salesPhones: [
-    { label: '+91 62844 83160', value: '+916284483160' },
-    { label: '+91 98146 33670', value: '+919814633670' },
-    { label: '+91 78892 02281', value: '+917889202281' },
-    { label: '+91 88473 56318', value: '+918847356318' },
-    { label: '+91 62840 12516', value: '+916284012516' },
-    { label: '+91 62747 94761', value: '+916274794761' }
+    { label: '+91 98211 15840', value: '+91 98211 15840s' },
+    { label: '+91 98211 15848', value: '+91 98211 15848' },
+  
   ],
   emails: [
     { label: 'sales@medralhealth.com', value: 'sales@medralhealth.com', primary: true },
-    { label: 'ceo@medral.com', value: 'ceo@medral.com', primary: false },
-    { label: 'finance@medral.com', value: 'finance@medral.com', primary: false }
+    { label: 'admin@medral.com', value: 'admin@medral.com', primary: false },
+
   ],
   primaryEmail: 'sales@medralhealth.com'
 };
--- config/offices.js
+++ config/offices.js
@@ -1,15 +1,7 @@
 module.exports = [
   {
-    name: 'Manufacturing Unit 1',
-    address: 'Plot No. D-173, Industrial Area 8B\nMohali – 160071, Punjab, India'
-  },
-  {
-    name: 'Manufacturing Unit 2',
-    address: 'Plot No. 45, Mega Food Park, Near South City\nLudhiana – 141008, Punjab, India'
-  },
-  {
     name: 'Corporate Office',
     address:
-      'First Floor 169, Spaze Platinum Tower, Sector 47,\nSohna Road, Gurugram, Haryana: 122018'
+      'First Floor 169, Spaze Platinum Tower, Sector-47,\nSohna Road, Gurugram, Haryana: 122018'
   }
 ];
